9042e9edaa0dec9c43f1364895352c91e0650567
masa <masa@gmail.com>
2019-03-14 08:55:59 +0000

    favorite-button

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -9,7 +9,8 @@
                 <div>
                     <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
                 </div>
-                <div>
+                <div id="button_wrapper">
+                    @include('favorites.favorites_button', ['micropost' => $micropost])
                     @if (Auth::id() == $micropost->user_id)
                         {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
                             {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
